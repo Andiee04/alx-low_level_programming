2ad4380b2ce61f36dffeeb0a5f7abf7ebd7face5
Marie-Andren <okoyemarieandren@gmail.com>
2023-04-21 21:32:11 -0700

    0x01 C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,14 @@ int main(void)
 
 	for (i = 10; i < 20; i++)
 		{
-			putchar((i % 10) + '0');
+		putchar((i % 10) + '0');
 	if (i != 19)
-	{
-			putchar(',');
-			putchar(' ');
-						
-	}
+		{
+		putchar(',');
+		putchar(' ');				
 		}
-			putchar('\n');
+		}
+		putchar('\n');
 
 	return (0);
 }
