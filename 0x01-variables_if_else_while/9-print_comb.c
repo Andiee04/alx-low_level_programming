bb0dc027dbbee5e19ba9a989ab2183ea5625426b
Marie-Andren <okoyemarieandren@gmail.com>
2023-04-21 21:07:02 -0700

    0x01 C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,23 @@
 
 /**
 * main - entry point
-*
-* Return: alway returns 0
-*
+* Return: alway returns 
 */
 
 int main(void)
 {
-int i;
+	int i;
 
 	for (i = 10; i < 20; i++)
-			{
-				putchar((i % 10) + '0');
+		{
+			putchar((i % 10) + '0');
 	if (i != 19)
 			{
-				putchar(',');
-				putchar(' ');
+			putchar(',');
+			putchar(' ');
 						
 			}
-			}
+		}
 				putchar('\n');
 	return (0);
 }
