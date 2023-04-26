9740bdc64e2f72765dc0bff4a50204c22792e475
Marie-Andren <okoyemarieandren@gmail.com>
2023-04-26 01:56:33 -0700

    debug

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,6 +6,7 @@
 */
 
 void jack_bauer(void)
+
 {
 int hours = 0;
 int min = 0;
@@ -13,7 +14,8 @@ int min = 0;
 	while (hours < 24)
 			{
 	while (min < 60)
-		{
+		
+	{
 				_putchar('0' + (hours / 10));
 				_putchar('0' + (hours % 10));
 				_putchar(':');
@@ -21,8 +23,10 @@ int min = 0;
 				_putchar('0' + (min % 10));
 				_putchar('\n');
 				min++;
-				}
+				
+	}
 			min = 0;
 			hours++;
-		}
+		
+			}
 }
