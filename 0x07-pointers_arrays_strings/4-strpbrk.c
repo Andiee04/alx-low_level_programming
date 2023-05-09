cfb99cac9b1bf60d70d8e678d2eaaa61b2163613
Marie-Andren <okoyemarieandren@gmail.com>
2023-05-08 18:32:11 -0700

    code added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,19 +12,19 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0') /*iterate through string*/
 	{
 
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++) /* iterate through target */
 		{
-			if (s[i] == accept[j]) 
+			if (s[i] == accept[j]) /* stop at first match */
 			{
-				s = &s[i];
+				s = &s[i]; /* set pointer to first occurence */
 				return (s);
 			}
 		}
 		i++;
 	}
-	return (NULL);
+	return (NULL); /* return NULL if no matches */
 
 }
