f8555bd3cd35759013d9976d8b2c77e037152c3b
Marie-Andren <okoyemarieandren@gmail.com>
2023-05-22 22:00:41 -0700

    finished

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * len - find length of string
+ * @str: string
+ * Return: length
+ */
+ 
+int len(char *str)
+{
+	int b;
+
+	for (b = 0; *(str + b); b++)
+		;
+	return (b);
+}
+
+/**
+ * strcpy - functions as a copy of the string pointed to by src,
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: source
+ */
+ 
+char *strcpy(char *dest, char *src)
+
+{
+	int b;
+
+	for (b = 0; b <= len(src); b++)
+		dest[b] = src[b];
+	return (dest);
+}
+/**
+ * new_dog - create new instance of struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Return: initialized instance of struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1;
+	char *copy_of_name;
+	char *copy_of_owner;
+
+	dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
+	if (dog1 == NULL)
+		return (NULL);
+
+	dog1->age = age;
+
+	/* make copies of struct members and validate, else free on error */
+	/* set values of struct members to copies of arguments or set to NULL */
+	if (name != NULL)
+	{
+		copy_of_name = malloc(len(name) + 1);
+		if (copy_of_name == NULL)
+		{
+			free(dog1);
+			return (NULL);
+		}
+		dog1->name = strcpy(copy_of_name, name);
+	}
+	else
+		dog1->name = NULL;
+
+	if (owner != NULL)
+	{
+		copy_of_owner = malloc(len(owner) + 1);
+		if (copy_of_owner == NULL)
+		{
+			free(copy_of_name);
+			free(dog1);
+			return (NULL);
+		}
+		dog1->owner = strcpy(copy_of_owner, owner);
+	}
+	else
+		dog1->owner = NULL;
+
+	return (dog1);
+}
