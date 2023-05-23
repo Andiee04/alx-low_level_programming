00d7d644df51a15beeecdcbe9bea6e912e84271a
Marie-Andren <okoyemarieandren@gmail.com>
2023-05-22 21:37:40 -0700

    fnail commit

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function  prints the name of the file
+ * using a common predefined macros
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
