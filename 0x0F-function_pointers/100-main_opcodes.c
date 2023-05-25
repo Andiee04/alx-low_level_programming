9b992cced3e7fc174e0600a038321b7d10c3a16e
Marie-Andren <okoyemarieandren@gmail.com>
2023-05-24 21:56:26 -0700

    b

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function with two arguments
+ * @argc: int type argument count
+ * @argv: char type argument array
+ * Return: 0
+ */
+ 
+int main(int argc, char **argv)
+{
+	int cal;
+
+	cal= 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (cal < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + cal));
+		cal++;
+		if (atoi(argv[1]) > cal)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
